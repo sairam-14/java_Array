92198f30e96bff3f08bec115b7c03d71cec5f3fa
sairam-14 <velpulasairam51@gmail.com>
2025-02-14 20:02:39 +0530

    Add files via upload

---
+++ Sumofelemnts.java
@@ -0,0 +1,12 @@
+class Sumofelemnts 
+{
+	public static void main(String[] args) 
+	{
+		int a[] ={10,-2,7,1,-19};
+		 int sum =0;
+		 for(int i=0;i<=4;i++){
+			 sum =sum+a[i];
+		 }
+		 System.out.println(sum);
+	}
+}
