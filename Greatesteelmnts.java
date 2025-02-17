20f29bcf53465bf9cad86492987a20c0a6b54087
sairam-14 <velpulasairam51@gmail.com>
2025-02-17 16:38:42 +0530

    Add files via upload

---
+++ Greatesteelmnts.java
@@ -0,0 +1,15 @@
+class Greatestelmnts 
+{
+	public static void main(String[] args) 
+	{
+		int a[]= {-6,-9,-14,-1,-3};
+
+		int max = a[0];
+		for(int i=0;i<=4;i++){
+			if(max<a[i]){
+				max=a[i];
+			}
+		}
+
+	}
+}
